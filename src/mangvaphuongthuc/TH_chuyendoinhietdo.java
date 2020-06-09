df537db1e0f6e5ac3218542bcd54e678befcb26e
nguyenmanhhung16021997 <nguyenmanhhung16021997@gmail.com>
2020-06-10 00:17:58 +0700

    demo

---
+++ src/mangvaphuongthuc/TH_chuyendoinhietdo.java
@@ -0,0 +1,41 @@
+package mangvaphuongthuc;
+
+import java.util.Scanner;
+
+public class TH_chuyendoinhietdo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        int choice;
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter fahrenheit: ");
+                    fahrenheit = input.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: "+ fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter celsius: ");
+                    double celcius = input.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: "+ celsiusToFahrenheit(celcius));
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0/5)*celsius+32;
+        return fahrenheit;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celcius = (5.0/9)*(fahrenheit-32);
+        return celcius;
+    }
+}
