47ef000c451b38705192dbafd3715de9595b3f6c
nguyenmanhhung16021997 <nguyenmanhhung16021997@gmail.com>
2020-06-10 00:03:47 +0700

    demo

---
+++ src/mangvaphuongthuc/TH_daonguoccacphantumang.java
@@ -0,0 +1,38 @@
+package mangvaphuongthuc;
+
+import java.util.Scanner;
+
+public class TH_daonguoccacphantumang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter a size:");
+            size = scanner.nextInt();
+            if (size>20) {
+                System.out.println("Size does not exceed 20");
+            }
+        } while (size>20);
+        array = new int[size];
+        int i = 0;
+        while (i<array.length) {
+            System.out.println("Enter element "+(i+1)+": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s","Element is array: ","");
+        for (int j=0;j<array.length;j++) {
+            System.out.print(array[j]+"\t");
+        }
+        for (int j=0;j<array.length/2;j++) {
+            int temp = array[j];
+            array[j] = array[size-1-j];
+            array[size-1-j] = temp;
+        }
+        System.out.printf("%-20s%s", "Reverse array: ","");
+        for (int j = 0;j < array.length;j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
+}
